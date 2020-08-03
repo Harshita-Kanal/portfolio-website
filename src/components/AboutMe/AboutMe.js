cd545cee2eed25954ce3fc85f87116e02fc72fa6
Harshita-Kanal <harshita.kgv@gmail.com>
2020-08-04 01:03:56 +0530

    Added typography

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -16,9 +16,9 @@ class About extends Component {
                     <br />
                     <br />
                     <ScrollAnimation animateIn="fadeIn">
-                    <p className="item">
+                    <p className= "flow-text">
                             Hey there, thanks for taking out the time to read through my portfolio
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ullamcorper nibh neque, at int<br />erdum augue rhoncus tempor. In vel fringilla libero, sit amet luctus nisl. Proin lobortis ullamcorper sapien laoreet venenatis. Aenean odio massa, <br /> molestie at arcu non, facilisis placerat turpis. Integer euismod commodo dui, nec iaculis ex semper at. Mauris suscipit <br />turpis vitae mattis porta. Duis vehicula eleifend eleifend. Proin fermentum purus quis erat semper, sodales mollis leo lacinia. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pretium, massa ut interdum varius, quam nunc ullamcorper nibh, quis euismod magna est malesuada nibh. Quisque aliquam suscipit blandit. Aenean ut pulvinar risus. Vestibulum a arcu euismod, fringilla mi in, efficitur leo. Proin ac sem turpis.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ullamcorper nibh neque, at interdum augue rhoncus tempor. In vel fringilla libero, sit amet luctus nisl. Proin lobortis ullamcorper sapien laoreet venenatis. Aenean odio massa, molestie at arcu non, facilisis placerat turpis. Integer euismod commodo dui, nec iaculis ex semper at. Mauris suscipit turpis vitae mattis porta. Duis vehicula eleifend eleifend. Proin fermentum purus quis erat semper, sodales mollis leo lacinia. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pretium, massa ut interdum varius, quam nunc ullamcorper nibh, quis euismod magna est malesuada nibh. Quisque aliquam suscipit blandit. Aenean ut pulvinar risus. Vestibulum a arcu euismod, fringilla mi in, efficitur leo. Proin ac sem turpis.
                     </p>
                     </ScrollAnimation>  
                 </div>
